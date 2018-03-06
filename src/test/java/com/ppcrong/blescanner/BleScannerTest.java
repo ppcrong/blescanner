86fde30c48f89c45c8fe2f8944d2dbb5e2aa1de7
ppcrong <ppcrong@gmail.com>
2018-03-06 14:16:53 +0800

    [ppcrong.20180306][blescanner] Modify BleScannerTest

--- src/test/java/com/ppcrong/blescanner/BleScannerTest.java
+++ src/test/java/com/ppcrong/blescanner/BleScannerTest.java
@@ -7,47 +7,18 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import java.util.Calendar;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-
 /**
  * MiscUtilsTest
  */
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 26)
-public class MiscUtilsTest {
+public class BleScannerTest {
     @Before
     public void setUp() throws Exception {
     }
 
     @After
     public void tearDown() throws Exception {
-    }
-
-    @Test
-    public void getIntToBitArrayString() throws Exception {
-
-        int expected;
-        String result;
-
-        expected = 0b11111111;
-        result = MiscUtils.getIntToBitArrayString(expected);
-        assertEquals(true, "11111111".equalsIgnoreCase(result));
-
-        expected = 0;
-        result = MiscUtils.getIntToBitArrayString(expected);
-        assertEquals(true, "00000000".equalsIgnoreCase(result));
-
-        expected = 0b10101010;
-        result = MiscUtils.getIntToBitArrayString(expected);
-        assertEquals(true, "10101010".equalsIgnoreCase(result));
-
-        expected = 0b01010101;
-        result = MiscUtils.getIntToBitArrayString(expected);
-        assertEquals(true, "01010101".equalsIgnoreCase(result));
     }
 
     @Test
