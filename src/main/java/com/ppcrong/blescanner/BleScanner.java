9b9915995695eb30c782cf163d7f5a25ee9a5828
ppcrong <ppcrong@gmail.com>
2018-03-09 18:12:27 +0800

    [ppcrong.20180309][blescanner] Add UUID filter parameter

--- src/main/java/com/ppcrong/blescanner/BleScanner.java
+++ src/main/java/com/ppcrong/blescanner/BleScanner.java
@@ -2,19 +2,21 @@ package com.ppcrong.blescanner;
 
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.UUID;
+
 /**
  * BleScanner
  */
 public class BleScanner {
 
-    public static void showScanner(AppCompatActivity activity) {
+    public static void showScanner(AppCompatActivity activity, UUID uuid) {
 
-        showDeviceScanningDialog(activity);
+        showDeviceScanningDialog(activity, uuid);
     }
 
     // region [Private Function]
-    private static void showDeviceScanningDialog(AppCompatActivity activity) {
-        final ScannerFragment dialog = ScannerFragment.getInstance(null); // Device that is advertising directly does not have the GENERAL_DISCOVERABLE nor LIMITED_DISCOVERABLE flag set.
+    private static void showDeviceScanningDialog(AppCompatActivity activity, UUID uuid) {
+        final ScannerFragment dialog = ScannerFragment.getInstance(uuid); // Device that is advertising directly does not have the GENERAL_DISCOVERABLE nor LIMITED_DISCOVERABLE flag set.
         dialog.show(activity.getSupportFragmentManager(), "scan_fragment");
     }
     // endregion [Private Function]
